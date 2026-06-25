3dfb4e72757aa375c627edf090e3d349b4ed2f74
Yahya Jabary <jabaryyahya@gmail.com>
2026-06-25 13:54:18 +0200

    Drop unused random init and its RNG helpers

--- Autograd/Utils.lean
+++ Autograd/Utils.lean
@@ -20,39 +20,6 @@ def arrApproxEq (a : Array Float) (b : Array Float) (tol : Float := 1e-9) : Bool
 
 /-!
 ===--------------------------------------------------------------------------===
-RNG
-===--------------------------------------------------------------------------===
--/
-
-private def rngNext (s : UInt64) : Float × UInt64 :=
-  let s' : UInt64 := 6364136223846793005 * s + 1442695040888963407
-  let u : Float := (s' >>> 11).toFloat / 9007199254740992.0
-  (u, s')
-
-private def rngGauss (mean stddev : Float) (s : UInt64) : Float × UInt64 :=
-  let (u1, s1) := rngNext s
-  let (u2, s2) := rngNext s1
-  let u1' := if u1 < 1e-300 then 1e-300 else u1
-  let z := Float.sqrt (-2.0 * Float.log u1') * Float.cos (2.0 * 3.141592653589793 * u2)
-  (mean + stddev * z, s2)
-
-def rngGaussFlat (r c : Nat) (σ : Float) (st : UInt64) : Array Float × UInt64 := Id.run do
-  let mut acc : Array Float := Array.mkEmpty (r * c)
-  let mut s := st
-  for _ in [0:r * c] do
-    let (x, s') := rngGauss 0.0 σ s
-    acc := acc.push x; s := s'
-  return (acc, s)
-
--- tests
-#guard approxEq (rngNext 42).1 (rngNext 42).1
-#guard let u := (rngNext 42).1; 0.0 ≤ u && u < 1.0
-#guard !approxEq (rngNext 42).1 (rngNext (rngNext 42).2).1
-#guard approxEq (rngGauss 0.0 0.08 7).1 (0.08 * (rngGauss 0.0 1.0 7).1)
-#guard (rngGaussFlat 3 4 0.08 1).1.size == 12
-
-/-!
-===--------------------------------------------------------------------------===
 Progress bar
 ===--------------------------------------------------------------------------===
 -/
--- MicroGPT/Model.lean
+++ MicroGPT/Model.lean
@@ -1,7 +1,6 @@
 import Autograd.Ops
 import Autograd.Tensor
 import Autograd.Optim
-import Autograd.Utils
 
 namespace MicroGPT
 open Autograd
@@ -178,35 +177,5 @@ def forward (p : Params) (input target : Array Nat) (mask : Array Float) (nEmbed
   let loss := forward p #[0, 1] #[0, 1] #[1, 1] 2 1
   let gm := loss.backward
   loss.shape == #[1, 1] && approxEq loss.data[0]! (-Float.log 0.5) && (gm.find? (fun e => e.1 == ParamIds.lmHead)).isSome
-
-/-!
-===--------------------------------------------------------------------------===
-Random init
-===--------------------------------------------------------------------------===
--/
-
-private def initParams (nLayer nEmbed blockSize vocabSize : Nat) (rng : UInt64) : Params × UInt64 := Id.run do
-  let σ : Float := 0.08
-  let mkLeaf (data : Array Float) (rows cols id : Nat) : Tensor :=
-    Tensor.leaf data rows cols id true
-  let (wte, r1) := rngGaussFlat vocabSize nEmbed σ rng
-  let (wpe, r2) := rngGaussFlat blockSize nEmbed σ r1
-  let (lmHead, r3) := rngGaussFlat nEmbed vocabSize σ r2
-  let mut r := r3
-  let mut blocks : Array TransformerBlock := #[]
-  for h in [0:nLayer] do
-    let (wq, r') := rngGaussFlat nEmbed nEmbed σ r; r := r'
-    let (wk, r') := rngGaussFlat nEmbed nEmbed σ r; r := r'
-    let (wv, r') := rngGaussFlat nEmbed nEmbed σ r; r := r'
-    let (wo, r') := rngGaussFlat nEmbed nEmbed σ r; r := r'
-    let (fc1, r') := rngGaussFlat nEmbed (4 * nEmbed) σ r; r := r'
-    let (fc2, r') := rngGaussFlat (4 * nEmbed) nEmbed σ r; r := r'
-    blocks := blocks.push { attnWq := mkLeaf wq nEmbed nEmbed (ParamIds.attnWq h), attnWk := mkLeaf wk nEmbed nEmbed (ParamIds.attnWk h), attnWv := mkLeaf wv nEmbed nEmbed (ParamIds.attnWv h), attnWo := mkLeaf wo nEmbed nEmbed (ParamIds.attnWo h), mlpFc1 := mkLeaf fc1 nEmbed (4 * nEmbed) (ParamIds.mlpFc1 h), mlpFc2 := mkLeaf fc2 (4 * nEmbed) nEmbed (ParamIds.mlpFc2 h) }
-  return ({ wte := mkLeaf wte vocabSize nEmbed ParamIds.wte, wpe := mkLeaf wpe blockSize nEmbed ParamIds.wpe, lmHead := mkLeaf lmHead nEmbed vocabSize ParamIds.lmHead, blocks := blocks }, r)
-
--- tests
-#guard let (p, _) := initParams 1 2 2 3 123; p.wte.data.size == 6 && p.wte.id == ParamIds.wte && p.wte.requiresGrad && p.blocks.size == 1 && p.blocks[0]!.mlpFc1.data.size == 16
-#guard arrApproxEq (initParams 1 2 2 3 123).1.wte.data (initParams 1 2 2 3 123).1.wte.data
-#guard !arrApproxEq (initParams 1 2 2 3 1).1.wte.data (initParams 1 2 2 3 2).1.wte.data
 
 end MicroGPT
